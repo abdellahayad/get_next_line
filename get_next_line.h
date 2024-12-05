59ad0798021a5e0d677677450583d5d7882f1836
Abdellah Ayad <aayadabdellah08@gmail.com>
2024-12-05 10:50:36 +0100

    init

--- get_next_line.h
+++ get_next_line.h
@@ -14,6 +14,5 @@ char *get_next_line(int fd);
 size_t ft_strlen(const char *str);
 char    *ft_strdup(char *s1);
 char *ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 # endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -47,27 +47,3 @@ char *ft_strjoin(char *s1, char *s2)
     dest[i] = '\0';
     return (dest);
 }
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-    size_t  i;
-    char *str;
-
-    if (!s)
-        return (NULL);
-    if (start > ft_strlen(s))
-        return (ft_strdup(""));
-    if (len > ft_strlen(s + start))
-        len = ft_strlen(s + start);
-    str = malloc((len + 1) * sizeof(char));
-    if (!str)
-        return (NULL);
-    i  = 0;
-    while (i < len)
-    {
-        str[i] = s[start + i];
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
-}
